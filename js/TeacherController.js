5cfd2778ce6cd80ae903c84c012d26cf3efaf0bd
lee kebler <student@epicodus.com>
2014-10-07 12:32:08 -0700

    Add edit ability in angular

--- js/TeacherController.js
+++ js/TeacherController.js
@@ -4,4 +4,8 @@ studentRoster.controller('TeachersCtrl', function TeachersCtrl($scope){
     $scope.teachers.push({"name": $scope.teacherName});
     $scope.teacherName = null
   };
+  $scope.deleteTeacher = function(teacher) {
+    var index = $scope.teachers.indexOf(teacher);
+    $scope.teachers.splice(index, 1);
+  };
 });
